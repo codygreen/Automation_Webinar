395721fc96f53a6cb8ecf20cc33fd0bc73d83d0e
Cody Green <cody@codygreen.com>
2019-04-24 08:04:27 -0500

    switch to CFT

--- code/3 - CICD/main.tf
+++ code/3 - CICD/main.tf
@@ -43,18 +43,38 @@ module "compute" {
   instance_count = 1
 }
 
-# Deploy BIG-IP
-module "big-ip" {
-  source = "github.com/codygreen/Terraform//modules/aws/big-ip/single-nic"
+resource "aws_cloudformation_stack" "bigip" {
+  name = "cicd-demo-bigip"
 
-  name               = "${var.name}"
-  vpc_id             = "${data.aws_vpc.cicd.id}"
-  vpc_cidr           = "${var.vpc_cidr}"
-  key_name           = "${var.ssh_key}"
-  subnet_id          = "${data.aws_subnet_ids.cicd.ids[0]}"
-  instance_count     = 1
-  allowed_mgmt_cidrs = ["${chomp(data.http.myIP.body)}/32"]
+  parameters = {
+    Vpc                     = "${data.aws_vpc.cicd.id}"
+    subnet1Az1              = "${data.aws_subnet_ids.cicd.ids[0]}"
+    imageName               = "Good25Mbps"
+    instanceType            = "m5.large"
+    sshKey                  = "${var.ssh_key}"
+    restrictedSrcAddress    = "${chomp(data.http.myIP.body)}/32"
+    restrictedSrcAddressApp = "0.0.0.0/0"
+    declarationUrl          = "https://raw.githubusercontent.com/codygreen/Automation_Webinar/master/code/3%20-%20CICD/as3_nginx.json"
+  }
+
+  template_url = "https://s3.amazonaws.com/f5-cft/f5-existing-stack-payg-1nic-bigip.template"
+  capabilities = ["CAPABILITY_IAM"]
 }
+
+# # Deploy BIG-IP - this doesn't include AS3 Service Discovery or latest cloudlibs 
+# module "big-ip" {
+#   source = "github.com/codygreen/Terraform//modules/aws/big-ip/single-nic"
+
+
+#   name               = "${var.name}"
+#   vpc_id             = "${data.aws_vpc.cicd.id}"
+#   vpc_cidr           = "${var.vpc_cidr}"
+#   key_name           = "${var.ssh_key}"
+#   subnet_id          = "${data.aws_subnet_ids.cicd.ids[0]}"
+#   instance_count     = 1
+#   allowed_mgmt_cidrs = ["${chomp(data.http.myIP.body)}/32"]
+# }
+
 
 # Configure BIG-IP - this is based on test methods that are not upstreamed yet
 # provider "bigip" {
