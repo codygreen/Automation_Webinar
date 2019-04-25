c536b8d58622ce65acdff154fe8a9d9b2caef359
Cody Green <cody@codygreen.com>
2019-04-25 08:44:35 -0500

    fix output dependency

--- code/3 - CICD/output.tf
+++ code/3 - CICD/output.tf
@@ -1,6 +1,11 @@
 #-------- output.tf --------
 output "private_ip" {
-  value = "${join(", ", aws_cloudformation_stack.bigip.*.outputs.Bigip1ExternalInterfacePrivateIp)}"
+  value = "${aws_cloudformation_stack.bigip.outputs["Bigip1ExternalInterfacePrivateIp"]}"
+
+  depends_on = [
+    # needed for CFT output
+    "aws_cloudformation_stack.bigip",
+  ]
 }
 
 output "password" {
